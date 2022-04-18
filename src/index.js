5c635db7105904f869c3a9778f3a363c4172fd57
Stefanos-Langkilde <83694853+Stefanos-Langkilde@users.noreply.github.com>
2022-04-18 21:56:02 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useState } from "react";
+import './index.css';
+import {FaStar} from "react-icons/fa";
+
+
+const createArray = (length) => [...Array(length)];
+
+function Star({selected=false, onSelect}) {
+  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect}/>;
+}
+
+function StarRating({totalStars = 5}){
+
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+    {createArray(totalStars).map((n, i) => (<Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)} />))}
+    <p>{selectedStars} of {totalStars}</p>
+    </>
+  );
+}
+
+function App() {
+
+  return (
+   <StarRating totalStars={5} />
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
